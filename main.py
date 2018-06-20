1f6985f7942b45fa4990d5ff204b18f01c53fd6e
JuanjoVG <juan.jose.vazquez.gimenez@fib.upc.edu>
2018-06-20 19:39:25 +0200

    Complete the example.

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ s5 = Segment('s5', 3., 7.)
 S = [s1, s2, s3, s4, s5]
 
 st = SegmentTree(S)
-print(st)
-print(st.search(0))
-print(st.search(2))
-print(st.search(5))
-print(st.search(10))
+assert st.search(0) == []
+assert st.search(1) == ['s1']
+assert st.search(3) == ['s1', 's2', 's5']
+assert st.search(4) == ['s2', 's5', 's1']
+assert st.search(4.1) == ['s2', 's5']
+assert st.search(5) == ['s2', 's5']
+assert st.search(5.99) == ['s5']
+assert st.search(6) == ['s5', 's3']
+assert st.search(7) == ['s5', 's3', 's4']
+assert st.search(8) == ['s3', 's4']
+assert st.search(10) == []
